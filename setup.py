84964d3772c3dbe7593b3e6ed2ac121836c93b62
João Costa <85129839+DarkZero2001@users.noreply.github.com>
2024-03-19 12:09:57 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 setup(
-    name='l_ozdm',
+    name='ozdm',
     packages=find_packages(),
     version='0.0.3',
     description='OpenZDM Library Functions and Classes',
